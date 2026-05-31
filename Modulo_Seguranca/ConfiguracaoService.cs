a01416b6376789326c91bbd9928eb3d09079c732
Felipe Siqueira Ramos Galvez <felipesrgalvez@gmail.com>
2026-05-31 04:49:18 -0400

    #4 Finishing the ConfiguracaoService logic to check and validate

--- Modulo_Seguranca/ConfiguracaoService.cs
+++ Modulo_Seguranca/ConfiguracaoService.cs
@@ -1,33 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
-using System.Xml.Serialization;
+﻿using System.Text.Json;
 
 namespace Modulo_Seguranca
 {
     public class ConfiguracaoService
     {
-        public Configuracao Carregar() 
+        public Configuracao Carregar()
         {
-            // Caminho absoluto do arquivo json
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
-            // Vai ler o arquivo de configuração
-            string text = File.ReadAllText(path);
-            // Converte o texto em um objeto da classe Configuracao, guardando as variaveis que estão definidas
-            Configuracao config = JsonSerializer.Deserialize<Configuracao>(text) ?? new Configuracao();
+            try
+            {
+                // Caminho absoluto do arquivo json
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
 
-            // Retorna o objeto de configuração com as keys do json
-            return config;
+                // Vai ler o arquivo de configuração
+                string text = File.ReadAllText(path);
+
+                // Converte o texto em um objeto da classe Configuracao, guardando as variaveis que estão definidas
+                Configuracao config = JsonSerializer.Deserialize<Configuracao>(text) ?? new Configuracao();
+
+                // Retorna o objeto de configuração com as keys do json
+                return config;
+            }
+
+            catch (FileNotFoundException)
+            {
+                throw new Exception("Arquivo de configuração não foi encontrado");
+            }
+
+            catch (JsonException)
+            {
+                throw new Exception("Erro, arquivo de configuração está com formato inválido ou dados inválidos");
+            }
         }
 
         public List<string> Validar(Configuracao config)
         {
-            // Cria a lista erros
-            List<string> erros = [];
+                // Cria a lista erros
+                List<string> erros = [];
 
-            // Adiciona um dicionário com as chaves e mensagens de erro para cada campo obrigatório
-            var fields = new Dictionary<string, string>
+                // Adiciona um dicionário com as chaves e mensagens de erro para cada campo obrigatório
+                var fields = new Dictionary<string, string>
             {
                 {config.Vigiagua.Email, "Email do Vigiagua não informado"},
                 {config.Vigiagua.Senha, "Senha do Vigiagua não informada"},
@@ -37,16 +48,17 @@ namespace Modulo_Seguranca
                 {config.Gal.Laboratorio, "Laboratório do Gal não informado"}
             };
 
-            // Para cada índice em fields, se a chave for vazia, ele vai retornar o valor daquuela chave que é a mensagem de erro
-            foreach (var i in fields) 
-            {
-                if (string.IsNullOrEmpty(i.Key))
+                // Para cada índice em fields, se a chave for vazia, ele vai retornar o valor daquela chave que é a mensagem de erro
+                foreach (var i in fields)
                 {
-                    erros.Add(i.Value);
+                    if (string.IsNullOrEmpty(i.Key))
+                    {
+                        erros.Add(i.Value);
+                    }
                 }
-            }
-            // Retorna a lista de erros
-            return erros;
+                // Retorna a lista de erros
+                return erros;
+            
         }
     }
 }

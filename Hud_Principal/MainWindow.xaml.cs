2f2a2b7046b2e1488610074d54c43c9d749342d9
Felipe Siqueira Ramos Galvez <01104397242@rondonia.local>
2026-06-11 10:11:23 -0400

    #6 feat: darksidebar with animated dropdown

--- Hud_Principal/MainWindow.xaml.cs
+++ Hud_Principal/MainWindow.xaml.cs
@@ -8,9 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Win32;
 
-using Conversor_de_Arquivos;
 
 namespace Hud_Principal
 {
@@ -21,9 +19,30 @@ namespace Hud_Principal
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ToggleSubmenu(StackPanel submenu)
         {
-
+            if (submenu.MaxHeight == 0)
+            {
+                submenu.Visibility = Visibility.Visible;
+                var anim = new System.Windows.Media.Animation.DoubleAnimation(0, 200, TimeSpan.FromSeconds(0.25));
+                submenu.BeginAnimation(FrameworkElement.MaxHeightProperty, anim);
+            }
+            else
+            {
+                var anim = new System.Windows.Media.Animation.DoubleAnimation(200, 0, TimeSpan.FromSeconds(0.2));
+                anim.Completed += (s, e) => submenu.Visibility = Visibility.Collapsed;
+                submenu.BeginAnimation(FrameworkElement.MaxHeightProperty, anim);
+            }
         }
+
+        private void BtnSisagua_Click(object sender, RoutedEventArgs e)
+            => ToggleSubmenu(SisaguaSubmenu);
+
+        private void BtnGal_Click(object sender, RoutedEventArgs e)
+            => ToggleSubmenu(GalSubmenu);
+
+        private void BtnIdaron_Click(object sender, RoutedEventArgs e)
+            => ToggleSubmenu(IdaronSubmenu);
+
     }
 }

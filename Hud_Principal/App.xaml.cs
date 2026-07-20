ca3babe0b3f6115f9e14a9a0bf123390b0b1a9cb
Felipe Siqueira Ramos Galvez <felipesrgalvez@gmail.com>
2026-07-20 19:32:31 -0400

    mudei a ordenação de pastas, tendo em vista que esse projeto será maior e irá crescer bem mais do que eu esperava

--- Hud_Principal/App.xaml.cs
+++ Hud_Principal/App.xaml.cs
@@ -1,15 +1,21 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.IO;
 using System.Windows;
 using Application = System.Windows.Application;
 
 namespace hud_principal
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Redireciona browsers do Playwright para pasta relativa ao .exe.
+            // Necessário em publish single-file: sem isso, Playwright busca em
+            // %LOCALAPPDATA%\ms-playwright, que não existe em máquina limpa.
+            Environment.SetEnvironmentVariable(
+                "PLAYWRIGHT_BROWSERS_PATH",
+                Path.Combine(AppContext.BaseDirectory, "playwright-browsers"));
 
+            base.OnStartup(e);
+        }
+    }
 }
